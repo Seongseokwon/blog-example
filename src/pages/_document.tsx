2cb20f3cf1f1111608edf8a987b2bfcc398bb75a
SeongSeokwon <pam9411@naver.com>
2024-02-15 23:28:28 +0900

    project setting

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,8 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ko">
       <Head />
       <body>
         <Main />
@@ -11,3 +11,6 @@ export default function Document() {
     </Html>
   );
 }
+ 
+
+//oXhsAtiYsiBS4afi
